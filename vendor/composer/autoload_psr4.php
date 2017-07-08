c4baa08d86a6eb77e8fdec8bb951da5878c92635
Wolfgang Moritz <wolfmoritz@gmail.com>
2017-07-08 13:40:23 -0700

    Composer update.

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,6 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'WolfMoritz\\Session\\' => array($vendorDir . '/wolfmoritz/session/src/Session'),
+    'Twig\\' => array($vendorDir . '/twig/twig/src'),
     'Slim\\Views\\' => array($vendorDir . '/slim/twig-view/src'),
     'Slim\\' => array($vendorDir . '/slim/slim/Slim'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
