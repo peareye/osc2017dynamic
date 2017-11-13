82fe5db35083b5eb787c9a940195351bed9710a8
Peri Moritz <peri.moritz@gmail.com>
2017-11-13 13:13:30 +0000

    2nd attempt at updating to bootstrap 4 beta.2. Edited the sass/1-tools/_bootstrap.scss file to incorporate changes to beta2 update. Ran composer to update the bootstrap vendor folder.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitc37607f50114ce92f1502074a14fce46
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitc37607f50114ce92f1502074a14fce46', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
