077b9ab43aa7d0a0aba7d0c003517e14210f626f
Wolfgang Moritz <wolfmoritz@gmail.com>
2018-04-08 09:31:43 +0100

    Added JS to prevent double click form submissions.

--- public/assets/admin/script.js
+++ public/assets/admin/script.js
@@ -1,20 +1,41 @@
+// jQuery plugin to prevent double submission of forms
+jQuery.fn.preventDoubleSubmission = function() {
+  $(this).on('submit',function(e){
+  	e.preventDefault();
+    var $form = $(this);
+
+    if ($form.data('submitted') === true) {
+      // Previously submitted - don't submit again
+      e.preventDefault();
+    } else {
+      // Mark it so that the next submit can be ignored
+      $form.data('submitted', true);
+    }
+  });
+
+  // Keep chainability
+  return this;
+};
+
+// Delete prompt handler
+$('body').on('click', '.deleteButton', function() {
+  var reply = confirm('Are you sure you want to delete?');
+  return reply;
+});
+
+// Show alert on request
+var showAlert = function(message, status = 'alert-info') {
+    $('.alert').removeClass('alert-info alert-success alert-danger alert-warning').addClass(status).find('.alert-message').html(message).end().show();
+}
+
+// Dismiss alert without deleting element
+$('.alert').on('click', '.alert-close', function() {
+    $(this).closest('.alert').hide().find('.alert-message').html('');
+});
+
+$('form').preventDoubleSubmission();
+
 $(document).ready(function() {
-	// Delete prompt handler
-	$('body').on('click', '.deleteButton', function() {
-	  var reply = confirm('Are you sure you want to delete?');
-	  return reply;
-	});
-
-	// Show alert on request
-	var showAlert = function(message, status = 'alert-info') {
-	    $('.alert').removeClass('alert-info alert-success alert-danger alert-warning').addClass(status).find('.alert-message').html(message).end().show();
-	}
-
-	// Dismiss alert without deleting element
-	$('.alert').on('click', '.alert-close', function() {
-	    $(this).closest('.alert').hide().find('.alert-message').html('');
-	});
-
 	if (window.location.pathname.match('showreviews')) {
 		var pending = parseInt($('#myTab span.badge').html());
 		if (pending > 0) {
--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,3 +1,23 @@
+// jQuery plugin to prevent double submission of forms
+jQuery.fn.preventDoubleSubmission = function() {
+  $(this).on('submit',function(e){
+    var $form = $(this);
+
+    if ($form.data('submitted') === true) {
+      // Previously submitted - don't submit again
+      e.preventDefault();
+    } else {
+      // Mark it so that the next submit can be ignored
+      $form.data('submitted', true);
+    }
+  });
+
+  // Keep chainability
+  return this;
+};
+
+$('form').preventDoubleSubmission();
+
 // Review Pagination. Interrupt page request and fetch reviews behind the scenes
 $('#review-set').on('click', '.pagination a', function(e) {
 	e.preventDefault();
